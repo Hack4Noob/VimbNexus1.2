cb0817cf321fbf6b96ff417e5335c21a9691d226
Hack4Noob <134813495+Hack4Noob@users.noreply.github.com>
2025-05-15 17:32:28 +0100

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
-  // Substitua com suas configuraÃ§Ãµes do Firebase
-  
-    apiKey: "AIzaSyAP3Sy1PB0m_EpKifOpUd7tc1_eAKF2alM",
-    authDomain: "vimbalambi-news.firebaseapp.com",
-    databaseURL: "https://vimbalambi-news-default-rtdb.firebaseio.com",
-    projectId: "vimbalambi-news",
-    storageBucket: "vimbalambi-news.appspot.com",
-    messagingSenderId: "212254517932",
-    appId: "1:212254517932:web:d061c943f90b4c4e204d09"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
